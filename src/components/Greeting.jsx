cdc0bc8131c9a4eb0370afbd28965454c4dd2aab
YTDev <belnisay@gmail.com>
2024-04-19 02:21:21 +0100

    adding react

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+// src/components/Greeting.jsx
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+    </div>
+  );
+};
+
+export default Greeting;
